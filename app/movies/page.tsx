071cd81e4561b0fd27e3410ea78716b83b29a766
Mahendra <mahendrabrowny@gmail.com>
2023-10-07 21:11:51 +0530

    protect movies routes

--- app/movies/page.tsx
+++ app/movies/page.tsx
@@ -1,9 +1,22 @@
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import React from "react";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 type Props = {};
 
-const page = (props: Props) => {
+const Movies = async (props: Props) => {
+  const supabase = createServerComponentClient({ cookies });
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  if (!session) {
+    redirect("/login");
+  }
+
   return <h1 className="text-red-400">Movieeeeeee</h1>;
 };
 
-export default page;
+export default Movies;
