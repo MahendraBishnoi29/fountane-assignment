54ae82c17c62b63077c418d179ea2b6ef8730f4c
Mahendra <mahendrabrowny@gmail.com>
2023-10-08 01:54:11 +0530

    fix auth callback route logic

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -4,16 +4,19 @@ import { NextResponse } from "next/server";
 
 import type { NextRequest } from "next/server";
 
+export const dynamic = "force-dynamic";
+
 export async function GET(request: NextRequest) {
   const requestUrl = new URL(request.url);
   const code = requestUrl.searchParams.get("code");
 
   if (code) {
-    const cookieStore = cookies();
-    const supabase = createRouteHandlerClient({ cookies: () => cookieStore });
+    const supabase = createRouteHandlerClient({ cookies });
     await supabase.auth.exchangeCodeForSession(code);
   }
 
   // URL to redirect to after sign in process completes
-  return NextResponse.redirect(requestUrl.origin);
+  return NextResponse.redirect(
+    `${requestUrl.origin}?success=Logged in successfully!`
+  );
 }
