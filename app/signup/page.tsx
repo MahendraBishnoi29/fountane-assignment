03e5a6e83fe9b2da1fe9834c4f086b742e990e87
Mahendra <mahendrabrowny@gmail.com>
2023-10-07 22:57:26 +0530

    submit form minor fix

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -109,7 +109,7 @@ const SignUp = (props: Props) => {
           <button
             disabled={loading}
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            type="button"
+            type="submit"
             onClick={handleSubmit(onSubmit)}
           >
             {loading ? "processing" : "Sign Up"}
