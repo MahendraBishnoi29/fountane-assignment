33d7e10b3a8f34a30f99c25f873a7f443f4374cc
Mahendra <mahendrabrowny@gmail.com>
2023-10-08 16:49:32 +0530

    title fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const dynamic = "force-dynamic";
 
 export const metadata: Metadata = {
-  title: "Movies Mania",
+  title: "Movies Paradise",
   description: "The Ultimate Movies Hub",
 };
 
